16336aa5c1dc23489727ded69f2a90ead44b940e
Guilherme Oliveira <ghophp@gmail.com>
2015-07-25 17:08:29 +0200

    tests are now working with a basic isolated database.. using the example-api sql to create on memory

--- api/entity_api_test.go
+++ api/entity_api_test.go
@@ -1,20 +1,74 @@
 package api
 
 import (
-	"github.com/DATA-DOG/go-sqlmock"
-	test "github.com/Onefootball/entity-rest-api/test"
+	"database/sql"
+	"fmt"
+	eram "github.com/Onefootball/entity-rest-api/manager"
+	erat "github.com/Onefootball/entity-rest-api/test"
+	"github.com/ant0ine/go-json-rest/rest"
+	_ "github.com/mattn/go-sqlite3"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
+var (
+	server  *httptest.Server
+	handler http.Handler
+)
+
+func init() {
+
+	db, err := sql.Open("sqlite3", ":memory:")
+
+	if err != nil {
+		log.Fatal("An error '%s' was not expected when opening a stub database connection", err)
+	} else {
+
+		dat, err := ioutil.ReadFile("./../example-api/blog.sql")
+		if err != nil {
+			log.Fatal("An error '%s' was not expected when opening sql file", err)
+		} else {
+			db.Exec(string(dat))
+		}
+	}
+
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+
+	entityManager := eram.NewEntityDbManager(db)
+	entityRestApi := NewEntityRestAPI(entityManager)
+
+	router, err := rest.MakeRouter(
+		rest.Get("/api/:entity", entityRestApi.GetAllEntities),
+		rest.Post("/api/:entity", entityRestApi.PostEntity),
+		rest.Get("/api/:entity/:id", entityRestApi.GetEntity),
+		rest.Put("/api/:entity/:id", entityRestApi.PutEntity),
+		rest.Delete("/api/:entity/:id", entityRestApi.DeleteEntity),
+	)
+
+	if err != nil {
+		log.Fatal("An error '%s' was not expected when creating the router", err)
+	}
+
+	api.SetApp(router)
+	handler = api.MakeHandler()
+
+	server = httptest.NewServer(handler)
+}
+
 func TestGETWithEmptySetShouldReturnEmptyJsonArray200(t *testing.T) {
+	recorded := erat.RunRequest(t, handler, erat.MakeSimpleRequest("GET", fmt.Sprintf("%s/api/user", server.URL), nil))
+	recorded.CodeIs(200)
+}
+
+func TestGETWithExistentSetShouldReturnJsonArray200(t *testing.T) {
 
 }
 
-func TestGETWithExistentSetShouldReturnJsonArray200() {
-
-}
-
-func TestGETEntityDoestExistsShouldReturn404() {
+func TestGETEntityDoestExistsShouldReturn404(t *testing.T) {
 
 }
 
